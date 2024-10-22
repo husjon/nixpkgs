9c909ca34b3eb853fbc1930fcb2d2c401779614f
Jon Erling Hustadnes <jonerling.hustadnes@gmail.com>
2024-10-22 22:12:37 +0200

    made the installation require a prefetch of the binary

--- pkgs/applications/graphics/pureref/default.nix
+++ pkgs/applications/graphics/pureref/default.nix
@@ -1,16 +1,28 @@
-{ lib, appimageTools, runCommand, curl, gnugrep, cacert }:
+{ lib, appimageTools, requireFile, }:
 
 appimageTools.wrapType1 rec {
   pname = "pureref";
   version = "2.0.3";
 
-  src = runCommand "PureRef-${version}_x64.Appimage" {
-    nativeBuildInputs = [ curl gnugrep cacert ];
-    outputHash = "sha256-0iR1cP2sZvWWqKwRAwq6L/bmIBSYHKrlI8u8V2hANfM=";
-  } ''
-    key="$(curl "https://www.pureref.com/download.php" --silent | grep '%3D%3D' | cut -d '"' -f2)"
-    curl -L "https://www.pureref.com/files/build.php?build=LINUX64.Appimage&version=${version}&downloadKey=$key" --output $out
-  '';
+  src = requireFile rec {
+    name = "PureRef-2.0.3_x64.Appimage";
+    hash = "sha256-0iR1cP2sZvWWqKwRAwq6L/bmIBSYHKrlI8u8V2hANfM=";
+    message = ''
+      In order to install PureRef, you must first comply to their license and contribution amount.
+      You may choose any amount, even free (select Custom and change the amount to \$0).
+
+      1. Go to https://pureref.com/download.php?forcePlatform=linux
+      2. Select Linux and choose Portable in the dropdown
+      3. Enter a contribution amount
+      4. Click the Download button
+
+      Once you have downloaded the file, please use the following
+      commands then re-run the installation:
+
+      cd ~/Downloads
+      nix-prefetch-url file://\$PWD/${name}
+    '';
+  };
 
   meta = with lib; {
     description = "Reference Image Viewer";
